329984f07c87832a7fda7bad3121e119c375435b
hftamayo <hftamayo@gmail.com>
2024-02-10 21:23:08 -0600

    healthcheck endpoint

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "github.com/gofiber/fiber/v2"
+)
 
-func main(){
+func main() {
     fmt.Print("This is a test")
+    app := fiber.New()
+
+    app.Get("/gotodo/healthcheck", func(c *fiber.Ctx) error {
+        return c.SendString("GoToDo RestAPI is up and running")
+    })
+
+    log.Fatal(app.Listen(":8001"))
 }
