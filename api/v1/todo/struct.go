673ea3e7752b048ad117fd1ed6f7afccb935f92c
Herbert Tamayo <hftamayo@gmail.com>
2024-04-21 08:14:31 -0600

    integrating struct and model

--- api/v1/todo/struct.go
+++ api/v1/todo/struct.go
@@ -1,8 +1,8 @@
 package todo
 
 type Todo struct {
-	Id    int    `json:"id"`
-	Title string `json:"title"`
-	Done  bool   `json:"done"`
-	Body  string `json:"body"`
+	Id    int    `gorm:"primary_key" json:"id"`
+	Title string `gorm:"type:varchar(100)" json:"title"`
+	Done  bool   `gorm:"default:false" json:"done"`
+	Body  string `gorm:"type:text" json:"body"`
 }
