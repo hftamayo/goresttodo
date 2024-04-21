9718c065679fccfa3cf2287c941ef66c765b7dbc
Herbert Tamayo <hftamayo@gmail.com>
2024-04-21 08:25:03 -0600

    working on todoHandlers

---
+++ api/v1/todo/handler.go
@@ -0,0 +1,26 @@
+package todo
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func HealthCheck(c *fiber.Ctx) error {
+}
+
+func CreateTodo(c *fiber.Ctx) error {
+}
+
+func UpdateTodo(c *fiber.Ctx) error {
+}
+
+func UpdateTodoDone(c *fiber.Ctx) error {
+}
+
+func GetAllTodos(c *fiber.Ctx) error {
+}
+
+func GetTodoById(c *fiber.Ctx) error {
+}
+
+func DeleteTodoById(c *fiber.Ctx) error {
+}
