39e1cc278208051bae410c9fcbaab1c142922a92
Herbert Tamayo <hftamayo@gmail.com>
2024-04-29 15:48:21 -0600

    updating createTodo method

--- api/v1/todo/handler.go
+++ api/v1/todo/handler.go
@@ -4,10 +4,15 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func HealthCheck(c *fiber.Ctx) error {
-}
-
 func CreateTodo(c *fiber.Ctx) error {
+	repo := &TodoRepositoryImpl{}
+	service := NewTodoService(repo)
+	todo := &Todo{}
+	err := service.CreateTodo(todo)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error: failed to create a new task": err.Error()})
+	}
+	return c.Status(fiber.StatusCreated).JSON(fiber.Map{"message": "task created successfully", "data": todo})
 }
 
 func UpdateTodo(c *fiber.Ctx) error {
