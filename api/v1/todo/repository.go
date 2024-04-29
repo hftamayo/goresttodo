f7f83c8b8119368bd16826ad393ab2045272ed2b
Herbert Tamayo <hftamayo@gmail.com>
2024-04-29 15:48:42 -0600

    updating the repo with the changes on handler's createTodo

--- api/v1/todo/repository.go
+++ api/v1/todo/repository.go
@@ -2,7 +2,7 @@ package todo
 
 type TodoRepository interface {
 	GetById(id int) (*Todo, error)
-	GetAll() ([]*Todo, error)
+	GetAll(page, pageSize int) ([]*Todo, error)
 	Create(todo *Todo) error
 	Update(todo *Todo) error
 	Delete(id int) error
