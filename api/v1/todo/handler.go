b7f0af4b8762fbedfc9cf6ee3b97cc0d318e4c3f
Herbert Tamayo <hftamayo@gmail.com>
2024-05-05 17:11:43 -0600

    fixing updateTodoDone workflow

--- api/v1/todo/handler.go
+++ api/v1/todo/handler.go
@@ -43,6 +43,30 @@ func UpdateTodo(c *fiber.Ctx) error {
 }
 
 func UpdateTodoDone(c *fiber.Ctx) error {
+	repo := &TodoRepositoryImpl{}
+	service := NewTodoService(repo)
+
+	// Parse the ID from the URL parameter.
+	id, err := c.ParamsInt("id")
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid ID"})
+	}
+
+	// Parse the updated todo from the request body.
+	todo := &Todo{}
+	if err := c.BodyParser(todo); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+
+	// Set the ID of the todo to the ID from the URL parameter.
+	todo.Id = id
+
+	err = service.MarkTodoAsDone(todo.Id) // Pass the ID of the todo instead of the todo itself.
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Failed to update task", "details": err.Error()})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"message": "Task updated successfully", "data": todo})
 }
 
 func GetAllTodos(c *fiber.Ctx) error {
