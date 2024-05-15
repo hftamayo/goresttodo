da0562830125e0c2b70f60bd346838d8c2cf8798
Herbert Tamayo <hftamayo@gmail.com>
2024-05-15 15:51:11 -0600

    removing id since form includes it in the default model struct

--- api/v1/models/user.go
+++ api/v1/models/user.go
@@ -7,9 +7,16 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+// the default gorm struct is this:
+// type Model struct {
+//     ID        uint `gorm:"primary_key"`
+//     CreatedAt time.Time
+//     UpdatedAt time.Time
+//     DeletedAt *time.Time `sql:"index"`
+// }
+
 type User struct {
 	gorm.Model
-	Id       int    `gorm:"primary_key" json:"id"`
 	Name     string `gorm:"type:varchar(50)" json:"name"`
 	Email    string `gorm:"type:varchar(50)" json:"email"`
 	Password string `gorm:"type:varchar(50)" json:"password"`
