4e970e4826e26a4325072d0ff5bd60858ecbd7f8
Herbert Tamayo <hftamayo@gmail.com>
2024-05-18 21:29:48 -0600

    revamping execution order on load

--- main.go
+++ main.go
@@ -16,33 +16,36 @@ func main() {
 	envVars, err := config.LoadEnvVars()
 	if err != nil {
 		log.Fatalf("Error loading environment variables: %v", err)
+	} else {
+		if !config.CheckDataLayerAvailability(envVars) {
+			log.Fatalf("Error: Data layer is not available: %v", err)
+		} else {
+			db, err := config.DataLayerConnect(envVars)
+			if err != nil {
+				log.Fatalf("Failed to connect to the database: %v", err)
+			} else {
+				app.Use(cors.New(cors.Config{
+					AllowOrigins:     "http://localhost:5173",
+					AllowHeaders:     "Origin, Content-Type, Accept",
+					AllowCredentials: true,
+				}))
+
+				app.Use(func(c *fiber.Ctx) error {
+					c.Locals("db", db)
+					return c.Next()
+				})
+
+				routes.SetupRoutes(app, db)
+
+				app.Get("/gotodo/healthcheck", func(c *fiber.Ctx) error {
+					return c.SendString("GoToDo RestAPI is up and running")
+				})
+
+				log.Fatal(app.Listen(":8001"))
+
+			}
+		}
+
 	}
 
-	if !config.CheckDataLayerAvailability(envVars) {
-		log.Fatalf("Error: Data layer is not available: %v", err)
-	}
-
-	db, err := config.DataLayerConnect(envVars)
-	if err != nil {
-		log.Fatalf("Failed to connect to the database: %v", err)
-	}
-
-	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5173",
-		AllowHeaders:     "Origin, Content-Type, Accept",
-		AllowCredentials: true,
-	}))
-
-	app.Use(func(c *fiber.Ctx) error {
-		c.Locals("db", db)
-		return c.Next()
-	})
-
-	routes.SetupRoutes(app, db)
-
-	app.Get("/gotodo/healthcheck", func(c *fiber.Ctx) error {
-		return c.SendString("GoToDo RestAPI is up and running")
-	})
-
-	log.Fatal(app.Listen(":8001"))
 }
