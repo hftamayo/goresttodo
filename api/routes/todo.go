de2124878eb8027a42753519cb4866fa5ead923b
Herbert Tamayo <hftamayo@gmail.com>
2024-05-23 14:52:19 -0600

    passing handler as the main para for data persistence

--- api/routes/todo.go
+++ api/routes/todo.go
@@ -3,34 +3,21 @@ package routes
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/hftamayo/gotodo/api/v1/todo"
-	"github.com/jinzhu/gorm"
 )
 
-const todoIDPath = "/gotodo/todo/:id"
+func SetupRoutes(app *fiber.App, handler *todo.Handler) {
+	const todoIDPath = "/gotodo/todo/:id"
 
-func SetupRoutes(app *fiber.App, db *gorm.DB) {
-	app.Post("/gotodo/todo", func(c *fiber.Ctx) error {
-		return todo.CreateTodo(c)
-	})
+	app.Post("/gotodo/todo", handler.CreateTodo)
 
-	app.Patch(todoIDPath+"/done", func(c *fiber.Ctx) error {
-		return todo.UpdateTodoDone(c)
-	})
+	app.Patch(todoIDPath+"/done", handler.UpdateTodoDone)
 
-	app.Patch(todoIDPath, func(c *fiber.Ctx) error {
-		return todo.UpdateTodo(c)
-	})
+	app.Patch(todoIDPath, handler.UpdateTodo)
 
-	app.Get("/gotodo/todos", func(c *fiber.Ctx) error {
-		return todo.GetAllTodos(c)
-	})
+	app.Get("/gotodo/todos", handler.GetAllTodos)
 
-	app.Get(todoIDPath, func(c *fiber.Ctx) error {
-		return todo.GetTodoById(c)
-	})
+	app.Get(todoIDPath, handler.GetTodoById)
 
-	app.Delete(todoIDPath, func(c *fiber.Ctx) error {
-		return todo.DeleteTodoById(c)
-	})
+	app.Delete(todoIDPath, handler.DeleteTodoById)
 
 }
