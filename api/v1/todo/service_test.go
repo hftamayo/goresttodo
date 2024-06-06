56c20a1666ba793939a112ff6748baac09109be1
Herbert Tamayo <hftamayo@gmail.com>
2024-06-06 14:27:57 -0600

    mocking the rest of the method from the interface

--- api/v1/todo/service_test.go
+++ api/v1/todo/service_test.go
@@ -17,6 +17,26 @@ func (m *MockTodoRepository) Create(todo *models.Todo) error {
 	return args.Error(0)
 }
 
+func (m *MockTodoRepository) GetById(id int) (*models.Todo, error) {
+	args := m.Called(id)
+	return args.Get(0).(*models.Todo), args.Error(1)
+}
+
+func (m *MockTodoRepository) GetAll(page, pageSize int) ([]*models.Todo, error) {
+	args := m.Called(page, pageSize)
+	return args.Get(0).([]*models.Todo), args.Error(1)
+}
+
+func (m *MockTodoRepository) Update(todo *models.Todo) error {
+	args := m.Called(todo)
+	return args.Error(0)
+}
+
+func (m *MockTodoRepository) Delete(id int) error {
+	args := m.Called(id)
+	return args.Error(0)
+}
+
 func TestTodoService_Create(t *testing.T) {
 	repo := new(MockTodoRepository)
 	todo := &models.Todo{Title: "Test Todo"}
