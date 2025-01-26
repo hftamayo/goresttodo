78cdd4d7d48cde93b7053f90aa9ca532ef18842c
hftamayo <hftamayo@gmail.com>
2025-01-26 07:11:58 -0600

    updating todo routes

--- api/routes/todo.go
+++ api/routes/todo.go
@@ -5,16 +5,16 @@ import (
 	"github.com/hftamayo/gotodo/api/v1/todo"
 )
 
-func SetupRoutes(app *gin.Engine, handler *todo.Handler) {
+func SetupTodoRoutes(app *gin.Engine, handler *todo.Handler) {
 	const todoIDPath = "/gotodo/todo/:id"
 
-	app.POST("/gotodo/todo", handler.CreateTodo)
+	app.POST("/gotodo/new", handler.CreateTodo)
 
 	app.PATCH(todoIDPath+"/done", handler.UpdateTodoDone)
 
 	app.PATCH(todoIDPath, handler.UpdateTodo)
 
-	app.GET("/gotodo/todos", handler.GetAllTodos)
+	app.GET("/gotodo/list", handler.GetAllTodos)
 
 	app.GET(todoIDPath, handler.GetTodoById)
 
