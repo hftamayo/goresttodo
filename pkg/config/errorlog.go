c2813139c8815bbaacadedb78a90402456c6efac
Herbert Tamayo <hftamayo@gmail.com>
2025-02-17 15:06:24 -0600

    setup file for redis logging

---
+++ pkg/config/errorlog.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"context"
+
+	"github.com/go-redis/redis/v8"
+)
+
+func ErrorLogConnect() *redis.Client {
+	redisClient := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	ctx := context.Background()
+	_, err := redisClient.Ping(ctx).Result()
+	if err != nil {
+		return nil, err
+	}
+	return redisClient, nil
+}
