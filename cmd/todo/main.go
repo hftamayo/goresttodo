1a353bfaf242ba078c220ebb2431638f571e4906
Herbert Tamayo <hftamayo@gmail.com>
2025-02-17 15:21:04 -0600

    adding redis client workflow

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -14,26 +14,32 @@ func main() {
 
 	r := gin.Default()
 	fmt.Printf("reading environment...\n")
-    envVars, err := config.LoadEnvVars()
-    if err != nil {
-        log.Fatalf("Error loading environment variables: %v", err)
-    }
+	envVars, err := config.LoadEnvVars()
+	if err != nil {
+		log.Fatalf("Error loading environment variables: %v", err)
+	}
 
-    fmt.Printf("verify data layer availability...\n")
-    db, err := config.CheckDataLayerAvailability(envVars)
-    if err != nil {
-        log.Fatalf("Error: Data layer is not available, exiting...: %v", err)
-    }
+	fmt.Printf("verify data layer availability...\n")
+	db, err := config.CheckDataLayerAvailability(envVars)
+	if err != nil {
+		log.Fatalf("Error: Data layer is not available, exiting...: %v", err)
+	}
 
-    fmt.Printf("connecting to the database...\n")
-    db, err = config.DataLayerConnect(envVars)
-    if err != nil {
-        log.Fatalf("Failed to connect to the database: %v", err)
-    }
+	fmt.Printf("connecting to the database...\n")
+	db, err = config.DataLayerConnect(envVars)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
 
-    fmt.Printf("connected to the database, loading last stage: \n")
-    routes.SetupRouter(r, db)
+	fmt.Printf("loading error logger...\n")
+	redisClient, err := config.ErrorLogConnect()
+	if err != nil {
+		log.Fatalf("Failed to connect to the error logger: %v", err)
+	}
 
-    fmt.Printf("GoToDo API is up and running\n")
-    log.Fatal(r.Run(":8001"))
-}	
+	fmt.Printf("connected to the database, loading last stage: \n")
+	routes.SetupRouter(r, db, redisClient)
+
+	fmt.Printf("GoToDo API is up and running\n")
+	log.Fatal(r.Run(":8001"))
+}
