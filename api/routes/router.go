ddac64e62af382c3819c075560198c0c45a1c267
Herbert Tamayo <hftamayo@gmail.com>
2025-02-17 15:21:20 -0600

    adding redisClient to the global route

--- api/routes/router.go
+++ api/routes/router.go
@@ -5,11 +5,12 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"github.com/hftamayo/gotodo/api/v1/task"
 	"gorm.io/gorm"
 )
 
-func SetupRouter(r *gin.Engine, db *gorm.DB) {
+func SetupRouter(r *gin.Engine, db *gorm.DB, redisClient *redis.Client) {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
