793097920532a61c8bb8b78b6d62995b5b0c0bc7
hftamayo <hftamayo@gmail.com>
2025-02-16 18:03:52 -0600

    renaming dir to task

--- api/v1/task/handler.go
+++ api/v1/task/handler.go
@@ -25,18 +25,18 @@ func (h *Handler) CreateTodo(c *gin.Context) {
 	db := h.Db
 	repo := NewTodoRepositoryImpl(db)
 	service := NewTodoService(repo)
-	todo := &models.Todo{}
+	task := &models.Task{}
 
-	if err := c.ShouldBindJSON(todo); err != nil {
+	if err := c.ShouldBindJSON(task); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot parse JSON"})
 		return
 	}
-	err := service.CreateTodo(todo)
+	err := service.CreateTodo(task)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create a new task", "details": err.Error()})
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H{"message": "task created successfully", "data": todo})
+	c.JSON(http.StatusCreated, gin.H{"message": "task created successfully", "data": task})
 }
 
 func (h *Handler) UpdateTodo(c *gin.Context) {
@@ -52,22 +52,22 @@ func (h *Handler) UpdateTodo(c *gin.Context) {
 	}
 
 	// Parse the updated todo from the request body.
-	todo := &models.Todo{}
-	if err := c.ShouldBindJSON(todo); err != nil {
+	task := &models.Task{}
+	if err := c.ShouldBindJSON(task); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot parse JSON"})
 		return
 	}
 
 	// Set the ID of the todo to the ID from the URL parameter.
-	todo.ID = uint(id)
+	task.ID = uint(id)
 
-	err = service.UpdateTodo(todo)
+	err = service.UpdateTodo(task)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update task", "details": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Task updated successfully", "data": todo})
+	c.JSON(http.StatusOK, gin.H{"message": "Task updated successfully", "data": task})
 }
 
 func (h *Handler) UpdateTodoDone(c *gin.Context) {
@@ -91,30 +91,30 @@ func (h *Handler) UpdateTodoDone(c *gin.Context) {
 	if !ok {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing 'done' field in request body"})
 	}
-	todo := &models.Todo{
+	task := &models.Task{
 		Model: gorm.Model{ID: uint(id)},
 		Done:  done,
 	}
 
-	todo, err = service.MarkTodoAsDone(int(todo.ID), done) // Pass the ID of the todo instead of the todo itself.
+	task, err = service.MarkTodoAsDone(int(task.ID), done) // Pass the ID of the todo instead of the todo itself.
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update task", "details": err.Error()})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Task updated successfully", "data": todo})
+	c.JSON(http.StatusOK, gin.H{"message": "Task updated successfully", "data": task})
 }
 
 func (h *Handler) GetAllTodos(c *gin.Context) {
 	db := h.Db
 	repo := NewTodoRepositoryImpl(db)
 	service := NewTodoService(repo)
-	todos, err := service.GetAllTodos()
+	tasks, err := service.list()
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to fetch tasks", " details": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "Tasks fetched successfully", "data": todos})
+	c.JSON(http.StatusOK, gin.H{"message": "Tasks fetched successfully", "data": tasks})
 }
 
 func (h *Handler) GetTodoById(c *gin.Context) {
@@ -129,12 +129,12 @@ func (h *Handler) GetTodoById(c *gin.Context) {
 		return
 	}
 
-	todo, err := service.GetTodoById(id)
+	task, err := service.GetTodoById(id)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch task", "details": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "Task fetched successfully", "data": todo})
+	c.JSON(http.StatusOK, gin.H{"message": "Task fetched successfully", "data": task})
 }
 
 func (h *Handler) DeleteTodoById(c *gin.Context) {
