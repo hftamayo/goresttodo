6760b58f6602f655afe71774ad457476c32bb6bc
Herbert Tamayo <hftamayo@gmail.com>
2025-02-18 14:35:58 -0600

    updating handler with error log and constant messages

--- api/v1/task/task_handler.go
+++ api/v1/task/task_handler.go
@@ -7,6 +7,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/hftamayo/gotodo/api/v1/errorlog"
 	"github.com/hftamayo/gotodo/api/v1/models"
+	"github.com/hftamayo/gotodo/pkg/utils"
 	"gorm.io/gorm"
 )
 
@@ -38,13 +39,13 @@ func (h *Handler) List(c *gin.Context) {
 
 		c.JSON(http.StatusBadRequest, gin.H{
 			"code":          http.StatusBadRequest,
-			"resultMessage": "OPERATION_FAILED",
+			"resultMessage": utils.OperationFailed,
 		})
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{
 		"code":          http.StatusOK,
-		"resultMessage": "OPERATION_SUCCESS",
+		"resultMessage": utils.OperationSuccess,
 		"tasks":         tasks,
 	})
 }
@@ -57,16 +58,27 @@ func (h *Handler) ListById(c *gin.Context) {
 	// Parse the ID from the URL parameter.
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		h.ErrorLogService.LogError("Task_list_by_id", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":          http.StatusBadRequest,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
-
 	task, err := service.ListById(id)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch task", "details": err.Error()})
+		h.ErrorLogService.LogError("Task_list_by_id", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":          http.StatusInternalServerError,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "Task fetched successfully", "data": task})
+	c.JSON(http.StatusOK, gin.H{
+		"code":          http.StatusOK,
+		"resultMessage": utils.OperationSuccess,
+		"task":          task,
+	})
 }
 
 func (h *Handler) Create(c *gin.Context) {
@@ -76,15 +88,27 @@ func (h *Handler) Create(c *gin.Context) {
 	task := &models.Task{}
 
 	if err := c.ShouldBindJSON(task); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot parse JSON"})
+		h.ErrorLogService.LogError("Task_create", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":          http.StatusBadRequest,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
 	err := service.Create(task)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to create a new task", "details": err.Error()})
+		h.ErrorLogService.LogError("Task_create", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":          http.StatusInternalServerError,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
-	c.JSON(http.StatusCreated, gin.H{"message": "task created successfully", "data": task})
+	c.JSON(http.StatusCreated, gin.H{
+		"code":          http.StatusCreated,
+		"resultMessage": utils.OperationSuccess,
+		"task":          task,
+	})
 }
 
 func (h *Handler) Update(c *gin.Context) {
@@ -95,14 +119,22 @@ func (h *Handler) Update(c *gin.Context) {
 	// Parse the ID from the URL parameter.
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		h.ErrorLogService.LogError("Task_update", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":          http.StatusBadRequest,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
 
 	// Parse the updated todo from the request body.
 	task := &models.Task{}
 	if err := c.ShouldBindJSON(task); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot parse JSON"})
+		h.ErrorLogService.LogError("Task_update", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":          http.StatusBadRequest,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
 
@@ -111,11 +143,19 @@ func (h *Handler) Update(c *gin.Context) {
 
 	err = service.Update(task)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update task", "details": err.Error()})
+		h.ErrorLogService.LogError("Task_update", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":          http.StatusInternalServerError,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Task updated successfully", "data": task})
+	c.JSON(http.StatusOK, gin.H{
+		"code":          http.StatusOK,
+		"resultMessage": utils.OperationSuccess,
+		"task":          task,
+	})
 }
 
 func (h *Handler) Done(c *gin.Context) {
@@ -126,18 +166,30 @@ func (h *Handler) Done(c *gin.Context) {
 	// Parse the ID from the URL parameter.
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		h.ErrorLogService.LogError("Task_done", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":          http.StatusBadRequest,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
 
 	var body map[string]bool
 	if err := c.ShouldBindJSON(&body); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Cannot parse JSON"})
+		h.ErrorLogService.LogError("Task_done", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":          http.StatusBadRequest,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
 	done, ok := body["done"]
 	if !ok {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Missing 'done' field in request body"})
+		h.ErrorLogService.LogError("Task_done", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":          http.StatusBadRequest,
+			"resultMessage": utils.OperationFailed,
+		})
 	}
 	task := &models.Task{
 		Model: gorm.Model{ID: uint(id)},
@@ -146,11 +198,19 @@ func (h *Handler) Done(c *gin.Context) {
 
 	task, err = service.Done(int(task.ID), done) // Pass the ID of the todo instead of the todo itself.
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update task", "details": err.Error()})
+		h.ErrorLogService.LogError("Task_done", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":          http.StatusInternalServerError,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"message": "Task updated successfully", "data": task})
+	c.JSON(http.StatusOK, gin.H{
+		"code":          http.StatusOK,
+		"resultMessage": utils.OperationSuccess,
+		"task":          task,
+	})
 }
 
 func (h *Handler) Delete(c *gin.Context) {
@@ -161,14 +221,25 @@ func (h *Handler) Delete(c *gin.Context) {
 	// Parse the ID from the URL parameter.
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid ID"})
+		h.ErrorLogService.LogError("Task_delete", err)
+		c.JSON(http.StatusBadRequest, gin.H{
+			"code":          http.StatusBadRequest,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
 
 	err = service.Delete(id)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete task", "details": err.Error()})
+		h.ErrorLogService.LogError("Task_delete", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"code":          http.StatusInternalServerError,
+			"resultMessage": utils.OperationFailed,
+		})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"message": "Task deleted successfully"})
+	c.JSON(http.StatusOK, gin.H{
+		"code":          http.StatusOK,
+		"resultMessage": utils.OperationSuccess,
+	})
 }
