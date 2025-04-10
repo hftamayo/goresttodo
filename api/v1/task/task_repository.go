e0a0151255554b6ed1412b4a9a247459d2d1513f
Herbert Tamayo <hftamayo@gmail.com>
2025-04-10 14:49:01 -0600

    renaming to task objects and ensuring impl will use repo file

--- api/v1/task/task_repository.go
+++ api/v1/task/task_repository.go
@@ -7,7 +7,10 @@ import (
 type TaskRepository interface {
 	List(page, pageSize int) ([]*models.Task, error)
 	ListById(id int) (*models.Task, error)
-	Create(todo *models.Task) error
-	Update(todo *models.Task) error
+	Create(task *models.Task) error
+	Update(task *models.Task) error
 	Delete(id int) error
 }
+
+// Ensure TaskRepositoryImpl implements TaskRepository at compile time
+var _ TaskRepository = (*TaskRepositoryImpl)(nil)
