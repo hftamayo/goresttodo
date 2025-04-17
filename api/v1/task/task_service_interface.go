51c400d9de839cfcfc35a333046493d4b34dcf48
Herbert Tamayo <hftamayo@gmail.com>
2025-04-17 17:59:53 -0600

    applying pagination on list interface

--- api/v1/task/task_service_interface.go
+++ api/v1/task/task_service_interface.go
@@ -3,7 +3,7 @@ package task
 import "github.com/hftamayo/gotodo/api/v1/models"
 
 type TaskServiceInterface interface {
-    List() ([]*models.Task, error)
+    List(page, pageSize int) ([]*models.Task, error)
     ListById(id int) (*models.Task, error)
     Create(task *models.Task) error
     Update(task *models.Task) error
