2deb5a7a10404a7226977c40f576e8363abc9975
Herbert Tamayo <hftamayo@gmail.com>
2025-04-22 09:43:11 -0600

    simplifying the task dto

--- api/v1/task/task_dto.go
+++ api/v1/task/task_dto.go
@@ -1,23 +1,24 @@
 package task
 
-import (
-	"github.com/hftamayo/gotodo/api/v1/models"
-)
-
-type TaskResponse struct {
-    ID          uint      `json:"id"`
-    Title       string    `json:"title"`
-    Description string    `json:"description"`
-    Done        bool      `json:"done"`
-    Owner       uint      `json:"owner"`
+type PaginationQuery struct {
+    Page     int `form:"page" binding:"required,min=1"`
+    PageSize int `form:"limit" binding:"required,min=1,max=100"`
 }
 
-func ToTaskResponse(task *models.Task) *TaskResponse {
-    return &TaskResponse{
-        ID:          task.ID,
-        Title:       task.Title,
-        Description: task.Description,
-        Done:        task.Done,
-        Owner:       task.Owner,
-    }
+type PaginationMeta struct {
+    Page     int `json:"page"`
+    PageSize int `json:"pageSize"`
+}
+
+type TaskListResponse struct {
+    Tasks      []*TaskResponse `json:"tasks"`
+    Pagination PaginationMeta  `json:"pagination"`
+}
+
+type TaskResponse struct {
+    ID          uint   `json:"id"`
+    Title       string `json:"title"`
+    Description string `json:"description"`
+    Done        bool   `json:"done"`
+    Owner       uint   `json:"owner"`
 }
