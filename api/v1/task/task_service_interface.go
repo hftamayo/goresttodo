4d91a5cc2a382a08688e8a1fdc6608bbbfc6ebe5
Herbert Tamayo <hftamayo@gmail.com>
2025-04-22 16:07:50 -0600

    updating task service iface with cursor pagination

--- api/v1/task/task_service_interface.go
+++ api/v1/task/task_service_interface.go
@@ -3,7 +3,7 @@ package task
 import "github.com/hftamayo/gotodo/api/v1/models"
 
 type TaskServiceInterface interface {
-    ListWithCursor(limit int, cursor string) ([]*models.Task, string, bool, error)
+    List(limit int, cursorStr string) ([]*models.Task, *CursorPaginationMeta, error)
     ListById(id int) (*models.Task, error)
     Create(task *models.Task) error
     Update(task *models.Task) error
