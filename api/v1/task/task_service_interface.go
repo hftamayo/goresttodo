58a0f9d5d0167fbedc7887acf7ea2cf87f2ee1d9
Herbert Tamayo <hftamayo@gmail.com>
2025-04-28 16:00:43 -0600

    fixing bugs on update and done workflows on task interface

--- api/v1/task/task_service_interface.go
+++ api/v1/task/task_service_interface.go
@@ -6,7 +6,7 @@ type TaskServiceInterface interface {
     List(cursor string, limit int) ([]*models.Task, string, int64, error)
     ListById(id int) (*models.Task, error)
     Create(task *models.Task) error
-    Update(task *models.Task) error
+    Update(id int, task *models.Task) error
     Delete(id int) error
-    Done(id int, done bool) (*models.Task, error)
+    Done(id int) (*models.Task, error)
 }
