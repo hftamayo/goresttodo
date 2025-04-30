77efa631e44514a00e067a0f7a516b5a88bcc59d
Herbert Tamayo <hftamayo@gmail.com>
2025-04-30 13:29:27 -0600

    extending cors setup

--- pkg/middleware/cors.go
+++ pkg/middleware/cors.go
@@ -8,22 +8,34 @@ import (
 func CORSMiddleware(config *config.EnvVars) gin.HandlerFunc {
     return func(c *gin.Context) {
         origin := c.Request.Header.Get("Origin")
+		allowed := false
 
         // Check if the origin is allowed
         for _, allowedOrigin := range config.FeOrigins {
             if origin == allowedOrigin {
                 c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+				allowed = true
                 break
             }
         }
         
         c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
         c.Writer.Header().Set("Access-Control-Max-Age", "86400") // 24 hours
-
+        c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
         // Handle preflight requests
         if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
+            if allowed {
+                c.AbortWithStatus(204)
+            } else {
+                c.AbortWithStatus(403)
+            }
+            return
+        }
+
+        // If origin is not allowed, abort with 403
+        if !allowed {
+            c.AbortWithStatus(403)
             return
         }
 
