78b36c95e805c87638b6f682f9743a40e42dffe9
Herbert Tamayo <hftamayo@gmail.com>
2025-05-21 15:00:41 -0600

    adding a helper for validationPerPagePagination

--- api/v1/task/task_validate_pagination.go
+++ api/v1/task/task_validate_pagination.go
@@ -19,5 +19,32 @@ func validatePaginationQuery(query CursorPaginationQuery) CursorPaginationQuery 
         query.Order = utils.DefaultOrder
     }
 
+    if query.Cursor != "" {
+        query.Cursor = strings.TrimSpace(query.Cursor)
+    }    
+
+    return query
+}
+
+func validatePagePaginationQuery(query PagePaginationQuery) PagePaginationQuery {
+    // Validate and set limit
+    if query.Limit <= 0 {
+        query.Limit = utils.DefaultLimit
+    }
+    if query.Limit > utils.MaxLimit {
+        query.Limit = utils.MaxLimit
+    }
+    
+    // Ensure page is at least 1
+    if query.Page <= 0 {
+        query.Page = 1
+    }
+    
+    // Normalize order to lowercase and set default
+    query.Order = strings.ToLower(query.Order)
+    if query.Order != "asc" && query.Order != "desc" {
+        query.Order = utils.DefaultOrder
+    }
+    
     return query
 }
