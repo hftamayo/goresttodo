429490b95fee5ec370f1b231bbbb65cd4593028c
Herbert Tamayo <hftamayo@gmail.com>
2025-05-21 14:51:40 -0600

    applying constants in validate pagination func

--- api/v1/task/task_validate_pagination.go
+++ api/v1/task/task_validate_pagination.go
@@ -2,19 +2,21 @@ package task
 
 import (
 	"strings"
+
+	"github.com/hftamayo/gotodo/pkg/utils"
 )
 
 func validatePaginationQuery(query CursorPaginationQuery) CursorPaginationQuery {
     if query.Limit <= 0 {
-        query.Limit = DefaultLimit
+        query.Limit = utils.DefaultLimit
     }
-    if query.Limit > MaxLimit {
-        query.Limit = MaxLimit
+    if query.Limit > utils.MaxLimit {
+        query.Limit = utils.MaxLimit
     }
     
     query.Order = strings.ToLower(query.Order)
     if query.Order != "asc" && query.Order != "desc" {
-        query.Order = DefaultOrder
+        query.Order = utils.DefaultOrder
     }
 
     return query
