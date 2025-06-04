9f599c4cf6230d55c86ea11e19e21596c62c6e8b
Herbert Tamayo <hftamayo@gmail.com>
2025-06-04 14:04:40 -0600

    implementing rate limiters

--- pkg/config/rate_limiter.go
+++ pkg/config/rate_limiter.go
@@ -3,10 +3,20 @@ package config
 import (
 	"time"
 
-	"github.com/redis/go-redis/v9"
 	"github.com/hftamayo/gotodo/pkg/utils"
+	"github.com/redis/go-redis/v9"
 )
 
-func SetupRateLimiter(redisClient *redis.Client, limit int, window time.Duration) *utils.RateLimiter {
-	return utils.NewRateLimiter(redisClient, limit, window)
+func SetupRateLimiter(redisClient *redis.Client, defaultLimit int, window time.Duration) *utils.RateLimiter {
+    rateLimiter := utils.NewRateLimiter(redisClient)
+    
+    // Set the window
+    rateLimiter.Window = window
+    
+    // Configure limits for different operation types
+    rateLimiter.SetLimitForOperation(utils.OperationRead, 100)     // 100 read requests per minute
+    rateLimiter.SetLimitForOperation(utils.OperationWrite, 30)     // 30 write requests per minute
+    rateLimiter.SetLimitForOperation(utils.OperationPrefetch, 200) // 200 prefetch requests per minute
+    
+    return rateLimiter
 }
