09b4badc45019e72ed502a4c500bd2cd4fabc794
Herbert Tamayo <hftamayo@gmail.com>
2025-06-10 08:40:28 -0600

    adding constants related to task handler

--- api/v1/task/constants.go
+++ api/v1/task/constants.go
@@ -4,5 +4,10 @@ const (
 	errTaskNotFoundFmt = "task with id %d not found"
 	errTaskReference = "task:%d"
 	errInvalidateCacheFmt = "Failed to invalidate cache: %v\n"
+	taskServiceListRef = "tasks:list"
+	headerLastModified = "Last-Modified"
+	headerCacheControl = "Cache-Control"
+	taskCacheKey = "task_%d"
+	taskPageCacheName = "task_page_*"
 
 )
