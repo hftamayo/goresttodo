b0929f8e5f867f53fabd5ed68b69ceb3898f3483
Herbert Tamayo <hftamayo@gmail.com>
2025-08-15 14:50:36 -0600

    updating router and main for dep injection

--- api/routes/router.go
+++ api/routes/router.go
@@ -2,23 +2,22 @@ package routes
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/hftamayo/gotodo/api/v1/errorlog"
 	"github.com/hftamayo/gotodo/api/v1/health"
 	"github.com/hftamayo/gotodo/api/v1/task"
-	"github.com/hftamayo/gotodo/pkg/utils"
-	"github.com/redis/go-redis/v9"
+	"github.com/hftamayo/gotodo/pkg/config"
 	"gorm.io/gorm"
 )
 
-func SetupRouter(r *gin.Engine, db *gorm.DB, redisClient *redis.Client, cache *utils.Cache) {
+func SetupRouter(r *gin.Engine, db *gorm.DB, cache config.CacheInterface, errorLogger config.ErrorLogger) {
 	
-	logRepo := errorlog.NewErrorLogRepositoryImpl(redisClient)
 	taskRepo := task.NewTaskRepositoryImpl(db)
 
-	errorLogService := errorlog.NewErrorLogService(logRepo)
-	taskService := task.NewTaskService(taskRepo, cache)
+	// Create task service with custom configuration
+	taskServiceConfig := task.DefaultTaskServiceConfig()
+	taskServiceConfig.ErrorLogger = errorLogger
+	taskService := task.NewTaskServiceWithConfig(taskRepo, cache, taskServiceConfig)
 
-	taskHandler := task.NewHandler(taskService, errorLogService, cache)
+	taskHandler := task.NewHandler(taskService)
 	healthHandler := health.NewHealthHandler(db)
 
 	SetupTaskRoutes(r, taskHandler)
